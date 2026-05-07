89dfbc7e019135e4e370368474a459f21d717ae8
omarrnaguibb <130666749+omarrnaguibb@users.noreply.github.com>
2026-05-07 14:53:47 +0300

    first commit

--- src/socketApi.js
+++ src/socketApi.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const api_route = "http://localhost:8080";
-// export const api_route = "https://arb-bk-see.onrender.com";
+// export const api_route = "http://localhost:8080";
+export const api_route =  "https://bop-bnk.onrender.com";
 
 export const socket = io(api_route);
